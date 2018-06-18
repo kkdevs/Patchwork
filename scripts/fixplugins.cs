ffb9fcd56449eb131adbeccdaed8b441b715503c
x <x@x>
2018-06-18 19:53:26 +0200

    fix bep4 plugins

--- scripts/fixplugins.cs
+++ scripts/fixplugins.cs
@@ -1,4 +1,4 @@
-﻿// compatibility for bepinex
+﻿//@INFO: Compatibility for plugins
 
 using System;
 using System.IO;
@@ -16,7 +16,7 @@ public class fixplugins : MonoBehaviour
 	public static bool hasBepinGo => bgo != null;
 	public static bool hasBepinAss =>
 		AppDomain.CurrentDomain.GetAssemblies().First(x => x.FullName.ToLower().StartsWith("bepinex")) != null;
-
+	
 	// bepinex can crash with us for multitude reasons. try to resuscite it, or
 	// at least the plugins.
 	void Awake()
@@ -90,137 +90,5 @@ public class fixplugins : MonoBehaviour
 					catch { };
 				}
 	}
-
-	// describe the BlockHeader
-	public class kkex
-	{
-		public static string Version => "3";
-		public static string BlockName = "KKEx";
-		public Dictionary<string, KeyValuePair<int, Dictionary<string, object>>> data;
-		public byte[] SaveBytes() => MessagePackSerializer.Serialize(data);
-		public void LoadBytes(byte[] buf, Version ver)
-		{
-			data = MessagePackSerializer.Deserialize<Dictionary<string, KeyValuePair<int, Dictionary<string, object>>>>(buf);
-		}
-	}
-
-	// one slot mapping stored by sideloader
-	[MessagePackObject(true)]
-	public class ResolveInfo
-	{
-		public string ModID;
-		public int Slot;
-		public string Property;
-	}
-
-	// import sideloader slot mappings and turn those into guid preference
-	// per <cat,id> pair.
-	public void RegisterBepinGuids(ChaFile f, kkex k)
-	{
-		var guids = k.data["com.bepis.sideloader.universalautoresolver"].Value["info"] as object[];
-		foreach (var entry in guids)
-		{
-			var e = MessagePackSerializer.Deserialize<ResolveInfo>(entry as byte[]);
-			var prop = e.Property;
-			var prop2 = prop.Split('.').Last();
-			print(prop);
-			print(prop2);
-			CategoryNo catno;
-			if (!getcat.TryGetValue(prop, out catno) && !getcat.TryGetValue(prop2, out catno))
-			{
-				print($"Unrecognized bepinex category mapping {e.Property}");
-				continue;
-			}
-			var prefs = f.dict.guidPrefs;
-			var pair = new KeyValuePair<int,int>((int)catno, e.Slot);
-			print(catno);
-			print(e.Slot);
-			List<string> chain;
-			if (!prefs.TryGetValue(pair, out chain))
-				prefs[pair] = chain = new List<string>();
-			if (!chain.Contains(e.ModID))
-				chain.Add(e.ModID);
-			print(prefs);
-		}
-	}
-
-	public void OnCardLoad(ChaFile f, BlockHeader bh, bool nopng, bool nostatus)
-	{
-		if (nopng) return;
-		print("Loading card");
-		var k = new kkex();
-		if (bh.Load(k))
-		{
-			print("Found KKEx");
-			// remember kkex so that we can save it back
-			f.dict.dict["kkex"] = k;
-			try
-			{
-				RegisterBepinGuids(f, k);
-			}
-			catch (Exception ex) { print(ex); };
-		}
-	}
-
-	// when card is saved (to a file, not savegame), dump back any bepinex
-	// info we've learned earlier into it
-	public void OnCardSave(ChaFile f, BinaryWriter w, List<object> blocks, bool nopng)
-	{
-		if (nopng) return;
-		var data = f.dict.Get<kkex>("kkex");
-		if (data != null)
-			blocks.Add(data);
-	}
-
-	// deobfuscate category
-	static Dictionary<string, CategoryNo> getcat = new Dictionary<string, CategoryNo>()
-	{
-			{ "cheekId", CategoryNo.mt_cheek},
-			{ "ClothesBot", CategoryNo.co_bot},
-			{ "ClothesBra", CategoryNo.co_bra},
-			{ "ClothesGloves", CategoryNo.co_gloves},
-			{ "ClothesJacketSubA", CategoryNo.cpo_jacket_a},
-			{ "ClothesJacketSubB", CategoryNo.cpo_jacket_b},
-			{ "ClothesJacketSubC", CategoryNo.cpo_jacket_c},
-			{ "ClothesPants", CategoryNo.co_panst},
-			{ "ClothesSailorSubA", CategoryNo.cpo_sailor_a},
-			{ "ClothesSailorSubB", CategoryNo.cpo_sailor_b},
-			{ "ClothesSailorSubC", CategoryNo.cpo_sailor_c},
-			{ "ClothesShoesInner", CategoryNo.co_shoes},
-			{ "ClothesShoesOuter", CategoryNo.co_shoes},
-			{ "ClothesShorts", CategoryNo.co_shorts},
-			{ "ClothesSocks", CategoryNo.co_socks},
-			{ "ClothesTop", CategoryNo.co_top},
-			{ "ChaFileBody.detailId", CategoryNo.mt_body_detail},
-			{ "ChaFileFace.detailId", CategoryNo.mt_face_detail},
-			{ "eyebrowId", CategoryNo.mt_eyebrow},
-			{ "eyelineDownId", CategoryNo.mt_eyeline_down},
-			{ "eyelineUpId", CategoryNo.mt_eyeline_up},
-			{ "eyeshadowId", CategoryNo.mt_eyeshadow},
-			{ "glossId", CategoryNo.mt_hairgloss},
-			{ "HairBack", CategoryNo.bo_hair_b},
-			{ "HairFront", CategoryNo.bo_hair_f},
-			{ "HairOption", CategoryNo.bo_hair_o},
-			{ "HairSide", CategoryNo.bo_hair_s},
-			{ "headId", CategoryNo.bo_head},
-			{ "hlDownId", CategoryNo.mt_eye_hi_down},
-			{ "hlUpId", CategoryNo.mt_eye_hi_up},
-			{ "lipId", CategoryNo.mt_lip},
-			{ "lipLineId", CategoryNo.mt_lipline},
-			{ "moleId", CategoryNo.mt_mole},
-			{ "nipId", CategoryNo.mt_nip},
-			{ "noseId", CategoryNo.mt_nose},
-			{ "ChaFileBody.PaintID1", CategoryNo.mt_body_paint},
-			{ "ChaFileFace.PaintID1", CategoryNo.mt_face_paint},
-			{ "ChaFileBody.PaintID2", CategoryNo.mt_body_paint},
-			{ "ChaFileFace.PaintID2", CategoryNo.mt_face_paint},
-			{ "PaintLayoutID1", CategoryNo.bodypaint_layout},
-			{ "PaintLayoutID2", CategoryNo.bodypaint_layout},
-			{ "Pupil1", CategoryNo.mt_eye},
-			{ "Pupil2", CategoryNo.mt_eye},
-			{ "sunburnId", CategoryNo.mt_sunburn},
-			{ "underhairId", CategoryNo.mt_underhair},
-			{ "whiteId", CategoryNo.mt_eye_white},
-	};
 }
 
